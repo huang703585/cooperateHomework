ef87cdd2d4da3facd0b3ff791e3659e10ffac2b2
huang <2419173819@qq.com>
2018-07-01 21:30:12 +0800

    at 7/1

---
+++ test/client.java
@@ -0,0 +1,11 @@
+
+public class client
+{
+    public static void main(String[] args)
+    {
+	csInterface a = new csInterface();
+	System.out.println(a.getMaintain());
+	System.out.println(a.getContact());
+	System.out.println(a.getAccount());
+    }
+}
